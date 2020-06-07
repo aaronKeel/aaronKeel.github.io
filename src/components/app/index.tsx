7cf63e477727987561d659e696a8b67135ea69e3
overtheline <aaronckeel@gmail.com>
2020-06-06 23:39:00 -0700

    menu

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,15 +1,29 @@
 import * as React from 'react';
 
 import { Hello } from '../hello';
-import { Thrashard } from '../lyrics';
+import { Thrashard, YouthAndLust, LyricsWrapperProps } from '../lyrics';
 
 import './style.scss';
 
+export interface ContentLookup {
+    'youth-and-lust': ({ className }: LyricsWrapperProps) => JSX.Element;
+    'thrashard': ({ className }: LyricsWrapperProps) => JSX.Element;
+}
+
+export const content = {
+    'youth-and-lust': YouthAndLust,
+    'thrashard': Thrashard,
+};
+
 export const App = (): JSX.Element => {
+    const [contentKey, setContentKey] = React.useState<keyof ContentLookup>('youth-and-lust');
+
+    const menuCB = (d: keyof ContentLookup) => setContentKey(d);
+
     return (
         <div className='grid-container'>
-            <Hello className='grid-item' />
-            <Thrashard className='grid-item' />
+            <Hello className='grid-item' menu={Object.keys(content)} menuCB={menuCB} />
+            {content[contentKey]({ className: 'grid-item' })}
             <div className='grid-item'></div>
         </div>
     );
--- src/components/hello/index.tsx
+++ src/components/hello/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 
-export interface HelloProps { className: string; }
+import './style.scss';
 
-export const Hello = ({ className }: HelloProps): JSX.Element => (
+export interface HelloProps {
+    className: string;
+    menu: string[];
+    menuCB: (d: string) => void;
+}
+
+export const Hello = ({ className, menu, menuCB }: HelloProps): JSX.Element => (
     <div className={className}>
         <span>Use your head.</span>
         <br></br>
         <span>aaronKeel.github.io</span>
+        {menu.map(d => (<p className='menu-item' key={d} onClick={() => menuCB(d)}>{d}</p>))}
     </div>
 );
