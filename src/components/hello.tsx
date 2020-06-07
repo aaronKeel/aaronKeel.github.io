36064ab65bba777a14ca2ff3f539f49069e05518
overtheline <aaronckeel@gmail.com>
2020-06-06 20:49:16 -0700

    text

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -4,6 +4,7 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export const Hello = ({ compiler, framework }: HelloProps): JSX.Element => (
     <div>
+        <p>Use your head.</p>
         <p>aaronKeel.github.io</p>
         <p>{compiler}</p>
         <p>{framework}</p>
